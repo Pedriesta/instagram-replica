760b70f1d9f9d0c11256e35fc55d7380ece24592
Pedreista <amarwaha@tekion.com>
2021-03-08 15:22:47 +0530

    Dynamic content enabled for profile info as well

--- script.js
+++ script.js
@@ -1,127 +1,166 @@
-const fetchResult = {
-    "name" : "tekioncorp",
-    "followers" : 346,
-    "following" : 500,
-    "numberOfPosts" : 12,
-    "posts" : [
-        {
-        "imageUrl" : "download.png",
-        "caption" : "Tekion Corp",
-        "likes" : 45,
-        "comments" : 50
-        },
-        {
-            "imageUrl" : "download.png",
+let bio = "";
+bio = bio + "Tekion".bold() + "<br>" + "\nSoftware Company<br>".italics() + "One platform that seamlessly connects your entire business.";
+
+function getFetchedData(){
+    const fetchResult = {
+        "name" : "tekioncorp",
+        "followers" : 346,
+        "following" : 500,
+        "numberOfPosts" : 12,
+        "posts" : [
+            {
+            "imageUrl" : "download5.jpeg",
             "caption" : "Tekion Corp",
             "likes" : 45,
             "comments" : 50
-        },
-        {
-            "imageUrl" : "download.png",
-            "caption" : "Tekion Corp",
-            "likes" : 45,
-            "comments" : 50
-        },
-        {
-            "imageUrl" : "download.png",
-            "caption" : "Tekion Corp",
-            "likes" : 45,
-            "comments" : 50
-        },
-        {
-            "imageUrl" : "download.png",
-            "caption" : "Tekion Corp",
-            "likes" : 45,
-            "comments" : 50
-        },
-        {
-            "imageUrl" : "download.png",
-            "caption" : "Tekion Corp",
-            "likes" : 45,
-            "comments" : 50
-        },
-        {
-            "imageUrl" : "download.png",
-            "caption" : "Tekion Corp",
-            "likes" : 45,
-            "comments" : 50
-        },
-        {
-            "imageUrl" : "download.png",
-            "caption" : "Tekion Corp",
-            "likes" : 45,
-            "comments" : 50
-        },
-        {
-            "imageUrl" : "download.png",
-            "caption" : "Tekion Corp",
-            "likes" : 45,
-            "comments" : 50
-        },
-        {
-            "imageUrl" : "download.png",
-            "caption" : "Tekion Corp",
-            "likes" : 45,
-            "comments" : 50
-        },
-        {
-            "imageUrl" : "download.png",
-            "caption" : "Tekion Corp",
-            "likes" : 45,
-            "comments" : 50
-        },
-    ],
-    "profilePicture" : "download.png",
-    "bio" : "Tekion"
+            },
+            {
+                "imageUrl" : "download1.jpeg",
+                "caption" : "Tekion Corp",
+                "likes" : 45,
+                "comments" : 50
+            },
+            {
+                "imageUrl" : "downloa2.jpeg",
+                "caption" : "Tekion Corp",
+                "likes" : 45,
+                "comments" : 50
+            },
+            {
+                "imageUrl" : "download3.jpeg",
+                "caption" : "Tekion Corp",
+                "likes" : 45,
+                "comments" : 50
+            },
+            {
+                "imageUrl" : "download4.jpeg",
+                "caption" : "Tekion Corp",
+                "likes" : 45,
+                "comments" : 50
+            },
+            {
+                "imageUrl" : "download5.jpeg",
+                "caption" : "Tekion Corp",
+                "likes" : 45,
+                "comments" : 50
+            },
+            {
+                "imageUrl" : "download6.jpeg",
+                "caption" : "Tekion Corp",
+                "likes" : 45,
+                "comments" : 50
+            },
+            {
+                "imageUrl" : "download1.jpeg",
+                "caption" : "Tekion Corp",
+                "likes" : 45,
+                "comments" : 50
+            },
+            {
+                "imageUrl" : "downloa2.jpeg",
+                "caption" : "Tekion Corp",
+                "likes" : 45,
+                "comments" : 50
+            },
+            {
+                "imageUrl" : "download3.jpeg",
+                "caption" : "Tekion Corp",
+                "likes" : 45,
+                "comments" : 50
+            },
+            {
+                "imageUrl" : "download4.jpeg",
+                "caption" : "Tekion Corp",
+                "likes" : 45,
+                "comments" : 50
+            },
+        ],
+        "profilePicture" : "download.png",
+        "bio" : bio
+    }
+
+    return fetchResult;
 }
 function fillData(){
     loadImages();
-    // loadProfilePicture();
-    // loadUserInfo();
+    loadProfilePicture();
+    loadUserInfo();
+
 }
 function loadImages(){
-    // fetch json from the server
-    // store it in the below variable
-    posts = fetchResult.posts;
+    posts = getFetchedData().posts;
     posts.forEach((post, i) => {
         // image container
-        let imageContainer = document.createElement("div");
-        imageContainer.className = "image_wrapper";
-
-        //like and comment icons
-        let likeIcon = document.createElement("span");
-        likeIcon.className = "material-icons";
-        likeIcon.innerHTML = "favorite_border";
-
-        let commentIcon = document.createElement("span");
-        commentIcon.className = "material-icons";
-        commentIcon.innerHTML = "chat_bubble";
-
-        let iconWrapper = document.createElement("div");
-        iconWrapper.className = "likes-comments";
-
-        iconWrapper.appendChild(likeIcon);
-        iconWrapper.appendChild(commentIcon);
-
-        //Image
-        let image = document.createElement("img");
-        image.setAttribute("src", post.imageUrl);
-        image.setAttribute("alt", toString(post.caption));
-        image.className = "gallery__img";
-
-        //wrap everything in the final container
-        imageContainer.appendChild(iconWrapper);
-        imageContainer.appendChild(image);
-
+        imageContainer = createImageWrapper(post);
         //finally add to the document
         document.getElementsByClassName("image-grid")[0].appendChild(imageContainer);
     });
 }
 
 function loadProfilePicture(){
-
+    imageUrl = getFetchedData().profilePicture;
+    let profilePicture = createProfilePicture(imageUrl);
+    document.getElementsByClassName("profile-picture-wrapper")[0].appendChild(profilePicture);
 }
 
 function loadUserInfo(){
+    const fetchResult = getFetchedData();
+    //set user name
+    document.getElementsByClassName("name")[0].innerHTML = fetchResult.name;
+    // set number of posts
+    document.getElementsByClassName("numberOfPosts")[0].innerHTML = fetchResult.numberOfPosts;
+    //set following
+    document.getElementsByClassName("following")[0].innerHTML = fetchResult.following;
+    // set number of followers
+    document.getElementsByClassName("followers")[0].innerHTML = fetchResult.followers;
 
+    document.getElementsByClassName("bio")[0].innerHTML = fetchResult.bio;
+}
+
+
+function createImageWrapper(post){
+    let imageContainer = document.createElement("div");
+    imageContainer.className = "image-wrapper";
+
+    //like and comment icons
+    
+    let iconWrapper = createLikeCommentIconWrapper();
+    //Image
+    let image = document.createElement("img");
+    image.setAttribute("src", post.imageUrl);
+    image.setAttribute("alt", toString(post.caption));
+    image.className = "gallery-img";
+
+    //wrap everything in the final container
+    imageContainer.appendChild(iconWrapper);
+    imageContainer.appendChild(image);
+
+    return imageContainer;
+}
+
+function createLikeCommentIconWrapper(){
+    let likeIcon = document.createElement("span");
+    likeIcon.className = "material-icons";
+    likeIcon.innerHTML = "favorite_border";
+
+    let commentIcon = document.createElement("span");
+    commentIcon.className = "material-icons";
+    commentIcon.innerHTML = "chat_bubble";
+
+    let iconWrapper = document.createElement("div");
+    iconWrapper.className = "likes-comments";
+
+    iconWrapper.appendChild(likeIcon);
+    iconWrapper.appendChild(commentIcon);
+
+    return iconWrapper;
+}
+
+function createProfilePicture(imageUrl){
+    let profilePicture = document.createElement("img");
+    profilePicture.className = "profile-picture";
+    profilePicture.setAttribute("alt", "Profile Picture");
+    profilePicture.setAttribute("src", imageUrl);
+
+    return profilePicture;
 }
