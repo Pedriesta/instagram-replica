fbc628f275fd3a9d2c3c4f0cbf0fd1962fb7cd58
Pedreista <amarwaha@tekion.com>
2021-03-19 16:22:37 +0530

    Folders created

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,11 @@
-// import {fillData} from './Generator.js';
-import {Model} from './Model.js';
-import {View} from './View.js';
-import {Controller} from './Controller.js';
+import {Model} from './Model/Model.js';
+import {View} from './View/View.js';
+import {Controller} from './Controller/Controller.js';
 
-//initialise view model and controller
-// call model.init before doing anything else
-// view.render
-// wrapped uop in wrapped up in  an async function
 
 (async function(){
     let model = new Model();
     let view = new View();
     let app = new Controller(model, view);
-    await app.model.init();
-    app.view.render(model);
-    app.view.attachEventListenersStaticElements();
+    app.init();
 })();
